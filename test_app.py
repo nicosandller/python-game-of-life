2290b3036fcf2b5dea0e4ef7c5285cbf02de351f
Nicolas Sandller <nico@kurv.co>
2015-11-02 17:55:41 -0300

    bug found on first test, FIXED

--- test_app.py
+++ test_app.py
@@ -6,14 +6,12 @@ game = Board(10)
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_pos_to_analyze(self):
+    def test_pos_to_analize(self):
         #We need to test that neither of the positions in the set that results from  pos_to_analize is outside dimensions
         xmax = game.dim
         ymax = game.dim
-        #Add a cell outside dimensions
-        game.add_cell(pos=(game.dim + 1 ,game.dim + 1))
-        #add a cell inside dimensions
-        game.add_cell(pos=(game.dim - 1 ,game.dim - 1))
+        #add a cell on the dimension limit (pos_to_analize should not return a pos outside dimensions)
+        game.add_cell(pos=(game.dim ,game.dim))
         #test that cells being returned are inside dimensions
         for pos in game.pos_to_analize():
             self.assertTrue(pos[0] <= xmax and pos[1] <= ymax)
